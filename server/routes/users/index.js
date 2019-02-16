4afcca99642f3a25e3422c65d305a12cae61a4a1
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-02-16 13:16:33 -0200

    Returning all users (local api)

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -2,92 +2,109 @@ const express = require('express')
 const Router = express.Router()
 const Joi = require('joi')
 
-//MOCK DATA
-const mock = {
-  users: [
-    {
-      id: 1,
-      name: 'Mario'
-    },
-    {
-      id: 2,
-      name: 'Brusarosco'
-    }
-  ]
-}
 
 // Common Functions
-const validateUser = req => {
-  const validationOptions = {
-    name: Joi.string()
-      .min(3)
-      .required()
-  }
 
-  return Joi.validate(req.body, validationOptions)
-}
 
-const getUser = req => {
-  return mock.users.find(user => {
-    return user.id === parseInt(req.params.id)
-  })
-}
+const User = require('../../models/User')
 
-Router.get('/:id', (req, res) => {
-  const user = getUser(req)
 
-  if (!user) {
-    return res.status(404).send('Invalid User Id')
-  }
+Router.get('/', async (req, res) => {
+	const allUsers = await User.find({
+		name: 'Walter White',
+		isPublished: true,
+	})
+	.limit(10)
+	.sort({ name: 1 }) // 1 means 'ASC' and -1 means -1
+	.select( { name: 1 })
 
-  return res.send(user)
+	res.send(allUsers)
 })
 
-Router.get('/', (req, res) => {
-  res.send(mock.users)
+
+Router.post('/', async (req, res) => {
+	const newUser = new User({
+		name: 'Mario Brusarosco',
+		email: 'mariobrusaroscog@gmail.com',
+		isPublished: true,
+	})
+
+	const result =  await newUser.save()
+
+	res.send(result)
 })
+// Common Functions
+// const validateUser = req => {
+//   const validationOptions = {
+//     name: Joi.string()
+//       .min(3)
+//       .required()
+//   }
 
-Router.post('/', (req, res) => {
-  // TODO:Two options of validation:
-  // -> 'joi' library
-  // ->  'Own validators'
-  validateUser(req)
-    .then(({ name }) => {
-      const user = {
-        id: mock.users.length + 1,
-        name
-      }
+//   return Joi.validate(req.body, validationOptions)
+// }
 
-      mock.users.push(user)
-      return res.send(mock.users)
-    })
-    .catch(({ details }) => res.status(404).send(details.map(error => error.message)))
-})
+// const getUser = req => {
+//   return mock.users.find(user => {
+//     return user.id === parseInt(req.params.id)
+//   })
+// }
 
-Router.put('/:id', (req, res) => {
-  const user = getUser(req)
+// Router.get('/:id', (req, res) => {
+//   const user = getUser(req)
 
-  if (!user) {
-    return res.status(404).send('Invalid User Id')
-  }
+//   if (!user) {
+//     return res.status(404).send('Invalid User Id')
+//   }
 
-  validateUser(req)
-    .then(({ name }) => {
-      user.name = name
-      return res.send(user)
-    })
-    .catch(({ details }) => res.status(404).send(details.map(error => error.message)))
-})
+//   return res.send(user)
+// })
 
-Router.delete('/:id', (req, res) => {
-  const user = getUser(req)
+// Router.get('/', (req, res) => {
+//   res.send(mock.users)
+// })
 
-  if (!user) {
-    return res.status(404).send('Invalid User Id')
-  }
+// Router.post('/', (req, res) => {
+//   // TODO:Two options of validation:
+//   // -> 'joi' library
+//   // ->  'Own validators'
+//   validateUser(req)
+//     .then(({ name }) => {
+//       const user = {
+//         id: mock.users.length + 1,
+//         name
+//       }
 
-  console.log('deleting user')
-  return res.send(user)
-})
+//       mock.users.push(user)
+//       return res.send(mock.users)
+//     })
+//     .catch(({ details }) => res.status(404).send(details.map(error => error.message)))
+// })
+
+// Router.put('/:id', (req, res) => {
+//   const user = getUser(req)
+
+//   if (!user) {
+//     return res.status(404).send('Invalid User Id')
+//   }
+
+//   validateUser(req)
+//     .then(({ name }) => {
+//       user.name = name
+//       return res.send(user)
+//     })
+//     .catch(({ details }) => res.status(404).send(details.map(error => error.message)))
+// })
+
+// Router.delete('/:id', (req, res) => {
+//   const user = getUser(req)
+
+//   if (!user) {
+//     return res.status(404).send('Invalid User Id')
+//   }
+
+//   console.log('deleting user')
+//   return res.send(user)
+// })
 
 module.exports = Router
