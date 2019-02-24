869e9595e76c28d470d459f97c8cdc3b655a7509
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-02-24 10:57:50 -0300

    Updating build

--- webpack/configuration/common.config.js
+++ webpack/configuration/common.config.js
@@ -1,7 +1,9 @@
+const path = require('path')
+
 module.exports = () => ({
 	// entry: [
-	// '@babel/polyfill',
-	// './src/index.js'
+	//   '@babel/polyfill',
+	//   './src/index.js'
 	// ],
 	// In case of multiple entry points
 	entry: {
@@ -10,5 +12,15 @@ module.exports = () => ({
 			'./src/index.js'
 		],
 		'second': './src/index-second.js'
-	}
+  },
+  resolve: {
+    // alias: {
+    //   "boilerplate-features": path.resolve('src','boilerplate-features')
+    // },
+    modules: [
+      path.resolve('src'),
+			path.resolve('src','boilerplate-features'),
+			path.resolve('node_modules'),
+    ]
+  }
 })
