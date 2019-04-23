8b9c14ea6f26b6b2e79b7ad56609784465d19a50
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-04-23 17:46:23 -0300

    Update schema.js

--- server/models/Language/schema.js
+++ server/models/Language/schema.js
@@ -1,6 +1,7 @@
 // Vendors
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const ObjectId = mongoose.Schema.Types.ObjectId
 
 const LanguageSchema = new Schema({
   name: String,
