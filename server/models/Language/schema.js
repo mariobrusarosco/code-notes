3f85ae54f406269f8048bdbe2830fdde2b5b6950
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-04-23 17:39:25 -0300

    Update schema.js

--- server/models/Language/schema.js
+++ server/models/Language/schema.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const LanguageSchema = new Schema({
-  name: String
+  name: String,
+  user: {
+    type: ObjectId,
+    ref: 'User'
+  },
 })
 
 module.exports = LanguageSchema
