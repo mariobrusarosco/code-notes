6b4f9238ece47a055dfa998eea8b69ae802b6392
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-04-30 12:18:44 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ const helmet = require('helmet')
 const morgan = require('morgan')
 
 app.use(morgan('tiny'))
-// app.use(helmet())
+app.use(helmet())
 app.use(cookieParser());
 
 // Custom Middlewares
@@ -52,7 +52,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   
   // For Authenticated Cookies
-//   res.cookie('username', 'Mario', { expires: new Date(Date.now() + 60000), 
+  res.cookie('username', 'Mario', { expires: new Date(Date.now() + 60000), 
   // res.header("Access-Control-Allow-Credentials", "true");
   next();
 });
