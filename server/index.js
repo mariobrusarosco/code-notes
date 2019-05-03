01d363c3e57b33e2d4b4d214a5abe09f67b5580f
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-05-03 00:47:26 -0300

    Consoling request

--- server/index.js
+++ server/index.js
@@ -55,17 +55,20 @@ app.use(cookieParser());
 const authorization = require('./middlewares/authorization')
 
 app.use(function(req, res, next) {
+  console.log(req)
+
   res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Methods','*');
 
   // // For Authenticated Cookies
-  res.cookie('username', 'ae', {
+  res.cookie('username', '1111111111111111', {
     expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-    secure: true,
+    // secure: true,
     httpOnly: true
   })
+
   next()
 });
 
