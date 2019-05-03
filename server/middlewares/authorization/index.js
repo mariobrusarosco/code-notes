0cfc7e9df73bce868ae41d1163942d6206d6d53b
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-05-03 10:40:12 -0300

    Temp Auth

--- server/middlewares/authorization/index.js
+++ server/middlewares/authorization/index.js
@@ -1,39 +1,48 @@
 const jwt = require('jsonwebtoken')
 const session = require('express-session')
 
-const authorizationByJWT = (req, res, next) => {
-  const givenToken = req.header('x-auth-token')
+// const authorizationByJWT = (req, res, next) => {
+//   const givenToken = req.header('x-auth-token')
 
-  if (!givenToken) {
-    return res
-      .status(401)
-      .send('Access Denined. No provided token')
-  }
+//   if (!givenToken) {
+//     return res
+//       .status(401)
+//       .send('Access Denined. No provided token')
+//   }
 
-  try {
-    const verifiedToken = jwt.verify(givenToken, process.env.JWT_SECRET)
-    req.verifiedUser = verifiedToken
-    next()
-  }
-  catch (error) {
-    return res.status(401).send('Invalid Token')
-  }
+//   try {
+//     const verifiedToken = jwt.verify(givenToken, process.env.JWT_SECRET)
+//     req.verifiedUser = verifiedToken
+//     next()
+//   }
+//   catch (error) {
+//     return res.status(401).send('Invalid Token')
+//   }
 
-}
+// }
 
 const authorization = (req, res, next) => {
-  const expiryDate = new Date( Date.now() + 60 * 60 * 1000 ); // 1 hour
-
-  session({
-    name: 'session',
-    keys: ['key1', 'key2'],
-    cookie: { secure: true,
-              httpOnly: true,
-              // domain: 'example.com',
-              path: 'foo/bar',
-              expires: expiryDate
-            }
-  })
+//   const expiryDate = new Date( Date.now() + 60 * 60 * 1000 ); // 1 hour
+    console.log('cookie: ', req.cookies.username)
+  
+  if(req.cookies.username == '1111111111111111' || req.cookies.username == '1111111111111111') {
+    console.log('authorized')
+  }
+  else {
+    return res
+      .status(401)
+      .send('Access Denined.')
+  }
+//   session({
+//     name: 'session',
+//     keys: ['key1', 'key2'],
+//     cookie: { secure: true,
+//               httpOnly: true,
+//               // domain: 'example.com',
+//               path: 'foo/bar',
+//               expires: expiryDate
+//             }
+//   })
 
   next()
 }
