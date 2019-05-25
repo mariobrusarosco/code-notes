822685e6ae28c5a47b8a0d838a0858359ccf93ea
Mario Brusarosco <mariobrusarosco@hotmail.com>
2019-05-25 16:55:12 -0300

    Updating Webpack

--- webpack/configuration/common.config.js
+++ webpack/configuration/common.config.js
@@ -1,14 +1,23 @@
 const path = require('path')
 
 const developmentConfig = () => ({
-  entry: [
-    '@babel/polyfill',
-    './src/index.js'
-  ],
+  // entry: [
+  //   '@babel/polyfill',
+  //   './src/index.js'
+  // ],
+  // In case of multiple entry points
+  entry: {
+    main: ['@babel/polyfill', './src/index.js'],
+    second: './src/index-second.js'
+  },
   resolve: {
+    // alias: {
+    //   "imagesPath": path.resolve('src','assets','images')
+    // },
     modules: [
       path.resolve('node_modules'),
       path.resolve('src'),
+      path.resolve('src', 'boilerplate-features')
     ]
   }
 })
